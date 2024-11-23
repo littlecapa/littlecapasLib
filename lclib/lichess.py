06f3b2c847cdddc741754723b8a0231b3e25dfc7
littlecapa <littlecapa@googlemail.com>
2024-11-23 16:41:56 +0100

    + Lichess Lib

---
+++ lclib/lichess.py
@@ -0,0 +1,13 @@
+import re
+
+def get_year_month_from_filename(filename):
+    try:
+        e = None
+        match = re.search(r'(\d{4})-(\d{2})', filename)
+        if match:
+            year = match.group(1)
+            month = match.group(2)
+            return int(year), int(month)
+    except Exception as e:
+        pass
+    raise Exception(f"Year, Month not found in filename {filename} {e}")
--- tests/test_gps.py
+++ tests/test_gps.py
@@ -1,4 +1,10 @@
+import unittest
 from lclib import gps
-def test_haversine():
-    assert gps.haversine(52.370216, 4.895168, 52.520008,
-    13.404954) == 946.3876221719836
+
+class TestGps(unittest.TestCase):
+    def test_haversine(self):
+        assert gps.haversine(52.370216, 4.895168, 52.520008,
+        13.404954) == 946.3876221719836
+
+if __name__ == '__main__':
+    unittest.main()
