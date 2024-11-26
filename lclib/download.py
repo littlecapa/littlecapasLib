e806bd9c9c84e4609a86b7edc9010637922d4f42
littlecapa <littlecapa@googlemail.com>
2024-11-26 14:58:19 +0100

    twic lib new designed

--- lclib/download.py
+++ lclib/download.py
@@ -12,11 +12,11 @@ def readZip(url, file_name_zip, attempts=1):
                 time.sleep(10)  # 10 seconds wait time between downloads
             with requests.get(url, headers={"User-Agent": ua}, stream=True) as response:
                 print ("Status: ", response.status_code)
-                if response.status_code == HTTP_ERROR:
+                if response.status_code == HTTPError:
                     print ("Ende")
                     return True
                 response.raise_for_status()
-                with open(file, 'wb') as out_file:
+                with open(file_name_zip, 'wb') as out_file:
                     print("File open")
                     for chunk in response.iter_content(chunk_size=1024*1024):  # 1MB chunks
                         out_file.write(chunk)
--- lclib/twic.py
+++ lclib/twic.py
@@ -1,46 +1,44 @@
-import os, zipfile
+import os, zipfile, requests
 from download import readZip
 
-class TwicWorker:
+def unzip_twic_file(self, filename_zip, unzip_folder):
+    with zipfile.ZipFile(filename_zip, 'r') as zip_file:
+        zip_file.extractall(unzip_folder)
+        os.remove(filename_zip)
+
+def get_file_info(self, issue_number):
+    filename_zip = self.twic_pattern.replace("<<number>>", str(issue_number))
+    return filename_zip, self.base_url+filename_zip
+
+def download_twic_file(self, issue_number: int):
     """
-    A class to handle TWIC (The Week in Chess) tasks.
-    This worker can download, process, and manage TWIC files.
+    Download a TWIC file for a given issue number.
+
+    Args:
+        issue_number (int): The TWIC issue number to download.
+
     """
-
-    def __init__(self, base_url: str, twic_pattern: str, download_dir: str, unzip_dir: str):
-        """
-        Initialize the TWIC worker.
-
-        Args:
-            base_url (str): The base URL for downloading TWIC files.
-            download_dir (str): The directory where TWIC files will be stored.
-        """
-        self.base_url = base_url.rstrip("/")
-        self.download_dir = download_dir
-        self.twic_pattern = twic_pattern
-        self.unzip_dir = unzip_dir
-        os.makedirs(self.download_dir, exist_ok=True)
-        os.makedirs(self.unzip_dir, exist_ok=True)
-
-    def unzip_twic_file(self, filename_zip, unzip_folder):
-        with zipfile.ZipFile(filename_zip, 'r') as zip_file:
-            zip_file.extractall(unzip_folder)
-            os.remove(filename_zip)
-
-    def download_twic_file(self, issue_number: int):
-        """
-        Download a TWIC file for a given issue number.
-
-        Args:
-            issue_number (int): The TWIC issue number to download.
-
-        """
-        filename_zip = self.twic_pattern.replace("<<number>>", str(issue_number))
-        filename = os.path.join(self.download_dir, filename_zip)
-        url = self.base_url+filename_zip
-        try:
-            readZip(url = url, filename = os.path.join(self.download_dir, filename))
-            self.unzip_twic_file(filename, self.unzip_dir)
-        except Exception as ex:
-            print ("error: ", str(ex.code))
-            raise ex
+    url, filename_zip = self.get_file_info(issue_number)
+    filename = os.path.join(self.download_dir, filename_zip)
+    try:
+        readZip(url = url, filename = os.path.join(self.download_dir, filename))
+        self.unzip_twic_file(filename, self.unzip_dir)
+    except Exception as ex:
+        print ("error: ", str(ex.code))
+        raise ex
+    
+def exist_twic_file(self, issue_number: int):
+    url, _ = self.get_file_info(issue_number)
+    try:
+        response = requests.head(url, allow_redirects=True)
+        # Check if the response status code indicates success (200 OK)
+        return response.status_code == 200
+    except requests.RequestException as e:
+        print(f"Error checking file at {url}: {e}")
+        return False
+    
+def get_highest_twic_issue(self):
+    highest = self.last_issue
+    while self.exist_twic_file(highest+1):
+        highest += 1
+    return highest
