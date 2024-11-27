b1d58a5d2681ab8135983b0f4dc3724edf31f18b
littlecapa <littlecapa@googlemail.com>
2024-11-27 15:21:06 +0100

    +Singleton Meta

---
+++ lclib/singleton.py
@@ -0,0 +1,14 @@
+#
+# Single Implenation according to: https://medium.com/django-unleashed/exploring-the-singleton-design-pattern-in-django-df796b181a13
+#
+class SingletonMeta(type):
+    """
+    A Singleton metaclass that ensures a class has only one instance.
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
